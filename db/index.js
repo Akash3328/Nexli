4d2deca3667dc1914ad57c0815875a9e01cd1585
Akash Odedara <akashodedara7@gmail.com>
2026-05-20 14:29:37 +0530

    chore: add pnpm-workspace.yaml to configure build settings

---
+++ db/index.js
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import {drizzle} from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+export const db = drizzle(postgres(process.env.DATABASE_URL));
+export default db;
